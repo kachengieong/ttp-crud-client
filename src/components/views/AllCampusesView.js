e59013418c8cec504441f83f48b9e76c548dc5d7
Ka Cheng Ieong <ieongkacheng@gmail.com>
2020-06-12 11:19:52 -0400

    make changes to views

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/AllCampusesView.css";
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 const AllCampusesView = (props) => {
   if (!props.allCampuses.length) {
